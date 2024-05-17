1b058d1f4157d3365aaef2fc045de02336d9dfba
MuhammadSoliman14 <musoliman14@gmail.com>
2024-05-17 14:56:13 +0300

    Fixed route handlers, signup, signin, did some ejs for the frontend

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const connectionString = process.env.MONGO_URI
 
 function connectDB(){ 
-    mongoose.connect(connectionString)
+    mongoose.connect('mongodb+srv://ahmedmokhtar2407:1234@cluster1.0ddauuc.mongodb.net/ecommerce')
     .then(console.log('database is connected'))
     .catch((err)=>{
         console.log('big failure')
--- models/User.js
+++ models/User.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const User = new mongoose.Schema({ // schema is constructor
-    name:{
-        type:String,
-        required:true,
-        unique:[true,'This username is taken'],
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: [true, 'This username is taken']
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true
     },
-    isAdmin:{
-        type:Boolean,
-        default:false,
+    isAdmin: {
+        type: Boolean,
+        default: false
     },
+}, { timestamps: true });
 
-},
-{timestamps : true}
-);
+const User = mongoose.model('User', UserSchema);
 
-module.exports = mongoose.model('User',User)
+module.exports = User;
