f9bc1113743ff8a576a510fb67235e6817f9e640
unknown <ahmedmokhtar2407@gmail.com>
2024-05-19 00:39:33 +0300

    frontend

---
+++ my-react-app/src/data.js
@@ -0,0 +1,52 @@
+    export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/12922525/pexels-photo-12922525.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/9293916/pexels-photo-9293916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://www.prada.com/content/dam/pradabkg_products/U/UCS/UCS438/16ASF031K/UCS438_16AS_F031K_S_231_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.400.500.webp",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://dfcdn.defacto.com.tr/7/C3629AX_24SP_NV71_09_01.jpg",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://dfcdn.defacto.com.tr/7/C3631AX_24SP_BK81_09_01.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
